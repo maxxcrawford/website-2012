0813378ba6bc4a3b0d1899540d18432e1b82386a
Maxx Crawford <mcrawford@broco.com>
2018-04-03 09:34:03 -0500

    Added back application JS include, formatted JS

--- public/js/application.js
+++ public/js/application.js
@@ -1,19 +1,19 @@
-$(document).ready(function() {
-    $("#toggle1").click(function() {
-        $(".target1").toggle('slow', function() {});
-    });
-    $("#toggle2").click(function() {
-        $(".target2").toggle('slow', function() {});
-    });
-    $("#toggle3").click(function() {
-        $(".target3").toggle('slow', function() {});
-    });
-    $("#toggle4").click(function() {
-        $(".target4").toggle('slow', function() {});
-    });
-    $("#bioShow").click(function() {
-        $(".bioBox").toggle('slow', function() {});
-    });
+$(document).ready(function () {
+  $('#toggle1').click(function () {
+    $('.target1').toggle('slow', function () {});
+  });
+  $('#toggle2').click(function () {
+    $('.target2').toggle('slow', function () {});
+  });
+  $('#toggle3').click(function () {
+    $('.target3').toggle('slow', function () {});
+  });
+  $('#toggle4').click(function () {
+    $('.target4').toggle('slow', function () {});
+  });
+  $('#bioShow').click(function () {
+    $('.bioBox').toggle('slow', function () {});
+  });
     // $(".fancybox").fancybox({
     //     'titlePosition': 'inside'
     // });
@@ -37,57 +37,53 @@ $(document).ready(function() {
     // });
 });
 
+$(document).ready(function () {
+  var $portfolioToggle = $('.portfolio article h2');
 
-$(document).ready(function() {
-    var $portfolioToggle = $('.portfolio article h2');
+  function portfolioToggle (activeItem) {
+    activeItem.closest('article').find('figure').slideToggle('slow');
+    activeItem.find('a').toggleClass('rotate');
+    activeItem.toggleClass('active');
+  }
 
-    function portfolioToggle(activeItem){
-        activeItem.closest('article').find('figure').slideToggle('slow');
-        activeItem.find('a').toggleClass('rotate');
-        activeItem.toggleClass('active');
+  $portfolioToggle.on('click touchstart', function (e) {
+    e.preventDefault();
+
+    var currActive = $portfolioToggle.filter('.active');
+
+    if ($(this).hasClass('active')) {
+      portfolioToggle($(this)); // Closes Clicked, Open Item
+    } else {
+      portfolioToggle(currActive); // Closes Active Item
+      portfolioToggle($(this)); // Opens Clicked Item
     }
+  });
 
-    $portfolioToggle.on('click touchstart', function(e) {
-        e.preventDefault();
+  function randomClassGen () {
+    var classNum = Math.floor(Math.random() * 6) + 1;
+    var className = 'gif-' + classNum;
+    return className;
+  }
 
-        var currActive = $portfolioToggle.filter('.active');
+  $('header h1').on('click touchstart', function (e) {
+    e.preventDefault();
 
-        if ( $(this).hasClass('active') ) {
-            portfolioToggle( $(this) ); // Closes Clicked, Open Item
-        } else {
-            portfolioToggle(currActive); //Closes Active Item
-            portfolioToggle( $(this) ); //Opens Clicked Item
-        }
-    });
+    var bodyClassCurr = $('body').attr('class').length;
 
-    function randomClassGen(){
-        var classNum = Math.floor(Math.random() * 6) + 1;
-        var className = "gif-" + classNum;
-        return className;
+    if (bodyClassCurr > 0) {
+      $('body').attr('class', '');
+    } else {
+      $('body').toggleClass(randomClassGen);
     }
+  });
 
-    $('header h1').on('click touchstart', function(e) {
-        e.preventDefault();
-
-        var bodyClassCurr = $('body').attr('class').length;
-
-        if (bodyClassCurr > 0){
-            $('body').attr('class', '');
-        } else {
-            $('body').toggleClass(randomClassGen);
-        }
-
-
-    });
-
-    $('.gallery-section').each(function() { // the containers for all your galleries
-        $(this).find('figure a').not('.external').magnificPopup({
+  $('.gallery-section').each(function () { // the containers for all your galleries
+    $(this).find('figure a').not('.external').magnificPopup({
             // delegate: 'a', // the selector for gallery item
-            type: 'image',
-            gallery: {
-                enabled: true
-            }
-        });
+      type: 'image',
+      gallery: {
+        enabled: true
+      }
     });
-
+  });
 });
